babd601cbf0823d680dd91b7d259bbe7d9ce607c
Alacce Cruz <alaccecruz151@gmail.com>
2023-01-27 20:59:57 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#form-habtis')
+const = nlwSetup = new NLWSetup(form)
+
+const data = {
+   run:['01-02', '01-02', '01-06']
+ 
+
+}
+
+nlwSetup.setdata (data)
+nlwSetup.load()
